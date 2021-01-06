28c9971ab8739143140b71ecb064623f066c2247
Lukas Bahrle <lukas.bahrle.santana@gmail.com>
2021-01-06 17:49:24 +0100

    create FeedCachePersistentContainer, subclass of NSPersistentContainer

---
+++ FeedStoreChallenge/CoreData/FeedCachePersistentContainer.swift
@@ -0,0 +1,12 @@
+//
+//  FeedCachePersistentContainer.swift
+//  FeedStoreChallenge
+//
+//  Created by Lukas Bahrle Santana on 06/01/2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class FeedCachePersistentContainer: NSPersistentContainer{}
