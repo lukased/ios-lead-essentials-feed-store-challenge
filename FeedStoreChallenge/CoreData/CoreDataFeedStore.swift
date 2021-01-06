6de479fe81df836e740bfac5b53ac6da2c6df983
Lukas Bahrle <lukas.bahrle.santana@gmail.com>
2021-01-06 18:33:20 +0100

    insert feed and retrieve non empty feed

--- FeedStoreChallenge/CoreData/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreData/CoreDataFeedStore.swift
@@ -28,11 +28,52 @@ public class CoreDataFeedStore: FeedStore{
 	}
 	
 	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+		self.managedObjectContext.perform { [weak self] in
+			guard let self = self else {return}
+			
+			let cache = ManagedCache(context: self.managedObjectContext)
+			cache.timestamp = timestamp
 		
+			for item in feed {
+				let feedImage = ManagedFeedImage(context: self.managedObjectContext)
+				feedImage.id = item.id
+				feedImage.imageDescription = item.description
+				feedImage.imageLocation = item.location
+				feedImage.imageURL = item.url
+				cache.addToImages(feedImage)
+			}
+			
+			completion(nil)
+		}
 	}
 	
 	public func retrieve(completion: @escaping RetrievalCompletion) {
-		completion(.empty)
+		let request: NSFetchRequest<ManagedCache> = ManagedCache.fetchRequest()
+		
+		managedObjectContext.perform {
+			do{
+				let cache = try request.execute()
+				
+				guard cache.count > 0 else{
+					completion(.empty)
+					return
+				}
+				
+				guard let firstCache = cache.last, let images = firstCache.images?.array as? [ManagedFeedImage], let timestamp = firstCache.timestamp else {
+					completion(.failure(NSError(domain: "Error", code: 0)))
+					return
+				}
+				
+				let localFeedImages: [LocalFeedImage] = images.compactMap{
+					guard let id = $0.id, let url = $0.imageURL else {return nil}
+					return LocalFeedImage(id: id, description: $0.imageDescription, location: $0.imageLocation, url: url)
+				}
+				completion(.found(feed: localFeedImages, timestamp: timestamp))
+			}
+			catch{
+				completion(.failure(error))
+			}
+		}
 	}
 }
 
